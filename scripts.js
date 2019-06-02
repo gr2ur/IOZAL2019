c8f221fbb60239e4868a1d356641f9d84743c0dd
AdamSerafin94 <51189690+AdamSerafin94@users.noreply.github.com>
2019-06-02 12:36:46 +0200

    Update scripts.js
    komentarze

--- scripts.js
+++ scripts.js
@@ -1,12 +1,14 @@
-function refreshTime()
+function refreshTime()  // funkcja służąca do odświeżania czasu
 {
 
     var t = new Date();
     var sec = t.getSeconds();
     var minutes = t.getMinutes();
     var hours = t.getHours();
-
-    if(sec < 10) sec = "0" + sec;
+                      
+    /* dodawanie warunków*/
+    
+    if(sec < 10) sec = "0" + sec;                 
     if(minutes < 10) minutes = "0" + minutes;
     if(hours < 10) hours = "0" + hours;
     
